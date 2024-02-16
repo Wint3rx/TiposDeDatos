42ec81e7fc3355788dfd79e2739580a3be75b63f
Winter._ <99782322+Wint3rx@users.noreply.github.com>
2024-02-16 17:11:05 -0600

    Add files via upload

---
+++ TiposDeDatos/TiposDeDatos/Program.cs
@@ -0,0 +1,34 @@
+﻿int n1 = 7;
+int n2 = 17;
+int suma;
+
+suma = n1 + n2;
+Console.WriteLine("El resultado de la suma de dos numeros enteros es: " + suma);
+
+decimal n3 = 7.23m;
+decimal n4 = 9.01m;
+decimal resta;
+
+resta = n3 - n4;
+Console.WriteLine("El resultado de la resta de dos numeros decimales es: " + resta);
+
+float n5 = 24.13f;
+float n6 = 18.46f;
+float multiplicacion;
+
+multiplicacion = n5 * n6;
+Console.WriteLine("El resultado de la multiplicacion de dos numeros flotantes es: " + multiplicacion);
+
+int n7 = 23;
+int n8 = 5;
+int division;
+
+division = n7 / n8;
+Console.WriteLine("El resultado de la division de dos numeros es: " + division);
+
+int n9 = 58;
+int n10 = 13;
+int modulo;
+
+modulo = n9 % n10;
+Console.WriteLine("El modulo de dos numeros enteros es: " + modulo);
